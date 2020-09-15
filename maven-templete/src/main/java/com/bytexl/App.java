6eb224f9fd6c90d578527da4fc71d3d05c9dca6d
nagagopi <nagagopi1826@outlook.com>
2020-09-15 14:43:20 +0000

    first commit

---
+++ maven-templete/src/main/java/com/bytexl/App.java
@@ -0,0 +1,13 @@
+package com.bytexl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
